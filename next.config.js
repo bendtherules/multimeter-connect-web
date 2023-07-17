ade11a93e6294f64a982b2e789a2d891b876faf8
bendtherules <abhasbhattacharya2@gmail.com>
2023-07-17 10:55:00 +0530

    Add PWA setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withPWA({
+  nextConfig,
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Multimeter speaker",
   description:
-    "Speaker for bluetooth DMMs like Aneng-9002, BSIDE ZT-300AB, ZOYI ZT-300AB, BABATools AD-900 ",
+    "Speaker for bluetooth DMMs like Aneng-9002, BSIDE ZT-300AB, ZOYI ZT-300AB, BABATools AD-900",
 };
 
 export default function RootLayout({
