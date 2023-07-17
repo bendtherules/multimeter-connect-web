dea3f607aae8962a8daf1bdff8d5ef8d9410b624
bendtherules <abhasbhattacharya2@gmail.com>
2023-07-17 12:39:09 +0530

    Update next-pwa settings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require("next-pwa")({
   dest: "public",
   skipWaiting: true,
   register: true,
   sw: "/sw.js",
+  runtimeCaching,
+  dynamicStartUrl: false,
 });
 
 /** @type {import('next').NextConfig} */
